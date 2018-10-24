f685e0a82d0d308efa008498ad7c6bf8035eb375
dualshockmaster <xdeividsabinox@live.com>
2018-10-23 22:43:36 -0300

    Finalização do login

--- registra_usuario.php
+++ registra_usuario.php
@@ -4,10 +4,51 @@ require_once('db.class.php');
 
 $usuario = $_POST['usuario'];
 $email = $_POST['email'];
-$senha = $_POST['senha'];
+$senha = md5($_POST['senha']);
 
 $objDb = new db();
 $link = $objDb->conecta_mysql();
+
+$usuario_existe = false;
+$email_existe = false;
+
+//verifica se o usuário já existe
+$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' ";
+if($resultado_id = mysqli_query($link,$sql)){
+    $dados_usuario =  mysqli_fetch_array($resultado_id);
+    if(isset($dados_usuario['usuario'])){
+        $usuario_existe = true;
+    }
+}else{
+    echo "erro ao tentar localizar o registro de usuário";
+}
+//verifica se o e-mail já existe
+$sql = "SELECT * FROM usuarios WHERE email = '$email' ";
+if($resultado_id = mysqli_query($link,$sql)){
+    $dados_usuario =  mysqli_fetch_array($resultado_id);
+    if(isset($dados_usuario['email'])){
+    $email_existe = true;
+    }
+}else{
+    echo "erro ao tentar localizar o registro de usuário";
+}
+
+
+if($usuario_existe || $email_existe){
+    $retorno_get = '';
+    if($usuario_existe){
+        $retorno_get.="erro_usuario=1&";
+    }
+    if($email){
+        $retorno_get.="erro_email=1&";
+    }
+    header('Location: inscrevase.php?'.$retorno_get);
+    die();
+}
+
+$sql = "SELECT * FROM usuarios WHERE usuario = '$usuario' ";
+
+
 $sql = "INSERT INTO usuarios (usuario, email, senha) values ('$usuario','$email','$senha')";
  
 //executar a query
--- validar_acesso.php
+++ validar_acesso.php
@@ -2,7 +2,7 @@
  session_start();
  require_once('db.class.php');
  $usuario = $_POST['usuario'];
- $senha = $_POST['senha'];
+ $senha = md5($_POST['senha']);
 
  $sql = "SELECT usuario,email FROM usuarios WHERE usuario = '$usuario' AND senha = '$senha'";
 
@@ -17,7 +17,7 @@
     if(isset($dados_usuario['usuario'])){
         $_SESSION['usuario'] = $dados_usuario['usuario'];
         $_SESSION['email'] = $dados_usuario['email'];
-        
+
         header('Location: home.php');
     }else {
         header('Location: index.php?erro=1');
